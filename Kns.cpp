e12cda1c195420e4aedbd34b395ddf5adcf53a01
Sowmittro Singha <soumitrasingha0070@gmail.com>
2026-06-10 13:29:19 +0600

    Kns.cpp

--- Kns.cpp
+++ Kns.cpp
@@ -1,56 +1,56 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h>
+using namespace namespace std;
 
-struct Item
-{
+struct Item {
     char name[10];
     int profit;
     int weight;
 };
 
-int main()
-{
-    int n, capacity;
-    cin >> n >> capacity;
+int main() {
+    int n;
+    cin >> n;
+
+    int cap;
+    cin >> cap;
 
     struct Item items[n];
-    for(int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cin >> items[i].name >> items[i].profit >> items[i].weight;
     }
 
-    vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
+    vector<vector<int>> dp(n + 1, vector<int>(cap + 1, 0));
 
-    for(int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++) {
         int w = items[i - 1].weight;
         int p = items[i - 1].profit;
-        for(int j = 0; j <= capacity; j++)
-        {
+
+        for (int j = 0; j <= cap; j++) {
             dp[i][j] = dp[i - 1][j];
-            if(w <= j)
+            if (w <= j) {
                 dp[i][j] = max(dp[i][j], dp[i - 1][j - w] + p);
+            }
         }
     }
 
+    int originalCap = cap;
     vector<string> selected;
-    int cap = capacity;
-    for(int i = n; i >= 1; i--)
-    {
-        if(dp[i][cap] != dp[i - 1][cap])
-        {
+    for (int i = n; i > 0; i--) {
+        if (dp[i][cap] != dp[i - 1][cap]) {
             selected.push_back(items[i - 1].name);
             cap -= items[i - 1].weight;
         }
     }
+
     reverse(selected.begin(), selected.end());
 
-    cout << "Max profit: " << dp[n][capacity] << endl;
-    cout << "Total selected: " << selected.size() << endl;
-    cout << "Selected items:" << endl;
-    for(int i = 0; i < selected.size(); i++)
-    {
+    cout << " Max profit : " << dp[n][originalCap] << endl;
+    cout << " Total selected : " << selected.size() << endl;
+
+    cout << " Selected items : ";
+    for (int i = 0; i < selected.size(); i++) {
         cout << selected[i] << " ";
     }
+    cout << endl;
     return 0;
 }
